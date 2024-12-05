c6059180f537a20de51c9b7ac0756502ec8386ba
Thiago <thi16thiago@gmail.com>
2024-12-05 09:51:00 -0300

    Atualizando configurações vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Escuta em todas as interfaces
+    port: 3002,       // Porta 3002
+  },
+});
